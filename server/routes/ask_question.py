4407b8b959e554c0adcaa909c1d37bddef32afaa
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-09-07 22:24:03 +0530

    ..

--- server/routes/ask_question.py
+++ server/routes/ask_question.py
@@ -23,7 +23,7 @@ async def ask_question(question: str = Form(...)):
         index = pc.Index(os.environ["PINECONE_INDEX_NAME"])
         embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         embedded_query = embed_model.embed_query(question)
-        res = index.query(vector=embedded_query, top_k=3, include_metadata=True)
+        res = index.query(vector=embedded_query, top_k=10, include_metadata=True)
 
         docs = [
             Document(
