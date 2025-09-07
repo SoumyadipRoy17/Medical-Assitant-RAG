309fa355b1a19819839ced9173f500d939553417
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-09-07 22:10:15 +0530

    ...

--- server/modules/llm.py
+++ server/modules/llm.py
@@ -10,8 +10,8 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 def get_llm_chain(retriever):
     llm = ChatGroq(
         groq_api_key=GROQ_API_KEY,
-        model="llama3-8b-8192",
-        
+        model="llama-3.3-70b-versatile",
+
     )
 
     prompt = PromptTemplate(
